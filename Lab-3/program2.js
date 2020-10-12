940c4518c7f1e66ad84f3ed8c2d0639765691e2a
Shreyas K <53894147+Shreyas999@users.noreply.github.com>
2020-10-12 16:53:48 +0530

    Create program2.js

---
+++ Lab-3/program2.js
@@ -0,0 +1,26 @@
+db.createCollection("Customers");
+
+db.Customers.insertMany([
+{cust_id:1,acc_bal:4000,acc_type:"savings"},
+{cust_id:2,acc_bal:900,acc_type:"current"},
+{cust_id:3,acc_bal:500,acc_type:"savings"},
+{cust_id:4,acc_bal:8000,acc_type:"current"},
+{cust_id:5,acc_bal:5000,acc_type:"savings"}]);
+
+db.Customers.find();
+
+db.Customers.find({acc_bal:{$gte:1200}, acc_type: "savings"});
+
+db.Customers.aggregate([{$group:
+    {
+        "_id": "$cust_id",
+        "maxBalance": {$max: "$acc_bal"},
+        "minBalance": {$min: "$acc_bal"},
+    }
+}]);
+
+mongoexport -d test -c Customers -f cust_id,acc_bal,acc_type --type csv -o Customerdetails.csv
+
+db.Customers.drop();
+
+mongoimport -d test -c Customers --type csv --file Customerdetails.csv --headerline
